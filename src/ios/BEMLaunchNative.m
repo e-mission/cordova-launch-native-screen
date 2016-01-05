f48a0ac333a28000999c0c67eb0836140821be24
K. Shankari <shankari@eecs.berkeley.edu>
2016-01-05 10:15:34 -0800

    Real implementation of the ios plugin
    The ios plugin was a bit more complicated than the android plugin. Loading the
    new view controller was easier, of course - we can just look it up by nib name.
    But simply showing the new view controller was not sufficient - since the
    parent of the cordova controller is not a nav controller, showing the new view
    replaced the existing view, which meant that there was no way to navigate back.
    
    I tried all the techniques for displaying a new view (showViewController,
    showDetailViewController, presentViewController), with a bunch of modal
    presentation styles, and none of them provided a graceful way to transition
    back.
    
    So I worked around this by adding a navigationController as a parent of the
    current view controller, and pushing the settings view controller onto it.
    
    This means that the native navbar hides the cordova navbar, so when we navigate
    back to the main screen, I hide the native navbar.
    
    An alternative approach would be to require that the native screen have a
    mechanism to dismiss itself. Since I wanted to ensure that this works with
    arbitrary native screens, I chose this approach, at the cost of making the
    cordova view controller a child of a nav controller.
    
    I am not sure why the view controller is not the child of a nav controller to
    begin with since that would make true hybrid apps so much easier.

--- src/ios/BEMLaunchNative.m
+++ src/ios/BEMLaunchNative.m
@@ -7,13 +7,61 @@
 
     NSString* callbackId = [command callbackId];
     NSString* controllerName = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Launched controller %@", controllerName];
 
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
+    @try {
+        UIWindow* keyWindow = [[UIApplication sharedApplication] keyWindow];
+        if (keyWindow == NULL) {
+            keyWindow = [[UIApplication sharedApplication] windows][0];
+        }
+        UIViewController* toStartView = [[UIViewController alloc] initWithNibName:controllerName bundle:nil];
+        keyWindow.rootViewController = toStartView;
+        
+        UINavigationController* navController = [[UINavigationController alloc] initWithRootViewController:self.viewController];
+        keyWindow.rootViewController = navController;
+        navController.delegate = self;
+        [navController pushViewController:toStartView animated:true];
+        NSString* msg = [NSString stringWithFormat: @"Launched controller %@", controllerName];
+        CDVPluginResult* result = [CDVPluginResult
+                                   resultWithStatus:CDVCommandStatus_OK
+                                   messageAsString:msg];
+        [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+    }
+    @catch (NSException* e) {
+        NSString* msg = [NSString stringWithFormat: @"While launching controller %@, error %@", controllerName, e];
+        CDVPluginResult* result = [CDVPluginResult
+                                   resultWithStatus:CDVCommandStatus_ERROR
+                                   messageAsString:msg];
+        [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+    }
+}
 
-    [self success:result callbackId:callbackId];
+- (void)navigationController:(UINavigationController *)navigationController
+       willShowViewController:(UIViewController *)viewController
+                    animated:(BOOL)animated
+{
+    NSLog(@"in willShowViewController, top view controller = %@, \
+          visible view controller = %@, \
+          new view controller = %@",
+          navigationController.topViewController,
+          navigationController.visibleViewController,
+          viewController);
+    if (viewController == self.viewController) {
+        [navigationController setNavigationBarHidden:TRUE];
+    } else {
+        [navigationController setNavigationBarHidden:FALSE];
+    }
+}
+
+- (void)navigationController:(UINavigationController *)navigationController
+      didShowViewController:(UIViewController *)viewController
+                    animated:(BOOL)animated
+{
+    NSLog(@"in didShowViewController, top view controller = %@, \
+          visible view controller = %@, \
+          new view controller = %@",
+          navigationController.topViewController,
+          navigationController.visibleViewController,
+          viewController);
 }
 
 @end
