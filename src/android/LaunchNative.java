a1f33f49fb2d63b9eba30cff1daa97fba6d2b4b3
K. Shankari <shankari@eecs.berkeley.edu>
2016-01-05 09:56:44 -0800

    Real implementation of the android plugin
    Assumptions:
    - Since the input name has to be consistent between android and iOS, we assume
      that the screenName is the class name of the activity.
    - But then we need to figure out the package that it is in. We assume that it
      is in the same package as the cordova activity.
    
    The rest of the implementation is pretty straightforward - we simply:
    - compute the fully qualified class name of the activity,
    - find its class,
    - and launch an intent using the class

--- src/android/LaunchNative.java
+++ src/android/LaunchNative.java
@@ -8,10 +8,20 @@ public class LaunchNative extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
         if (action.equals("launch")) {
+            java.lang.Package pkg = cordova.getClass().getPackage();
+            System.out.println("cordova has package name "+pkg);
             String activityName = data.getString(0);
-            callbackContext.success("Launched activity "+activityName);
+            String fqcn = pkg.getName()+"."+activityName;
+            System.out.println("FQCN = "+pkg.getName() + fqcn);
+            try {
+                Class fqClass = Class.forName(fqcn);
+                Intent startIntent = new Intent(cordova.getActivity(), fqClass);
+                cordova.getActivity().startActivity(startIntent);
+                callbackContext.success("Launched activity " + activityName);
+            } catch (ClassNotFoundException e) {
+                callbackContext.error("Class "+fqcn+" not found, unable to launch");
+            }
             return true;
         } else {
             return false;
