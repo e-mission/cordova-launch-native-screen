5392bd974ed2dca92b275e2d822f07978ec2430e
K. Shankari <shankari@eecs.berkeley.edu>
2016-01-05 21:18:11 -0800

    Read the correct package name
    We can't read the package name from the cordova interface since that is in the
    org.apache.cordova package. Instead, we get it from the cordova activity. So as
    long as the new screen is in the same package as the cordova activity, we are
    fine.

--- src/android/LaunchNative.java
+++ src/android/LaunchNative.java
@@ -10,17 +10,18 @@ public class LaunchNative extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("launch")) {
-            java.lang.Package pkg = cordova.getClass().getPackage();
-            System.out.println("cordova has package name "+pkg);
+            String pkgName = cordova.getActivity().getPackageName();
+            System.out.println("cordova has package name "+pkgName);
             String activityName = data.getString(0);
-            String fqcn = pkg.getName()+"."+activityName;
-            System.out.println("FQCN = "+pkg.getName() + fqcn);
+            String fqcn = pkgName+"."+activityName;
+            System.out.println("FQCN = "+ fqcn);
             try {
                 Class fqClass = Class.forName(fqcn);
                 Intent startIntent = new Intent(cordova.getActivity(), fqClass);
                 cordova.getActivity().startActivity(startIntent);
                 callbackContext.success("Launched activity " + activityName);
             } catch (ClassNotFoundException e) {
+                e.printStackTrace();
                 callbackContext.error("Class "+fqcn+" not found, unable to launch");
             }
             return true;
